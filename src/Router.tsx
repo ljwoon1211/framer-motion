2c345305b1f975cbc42f7c98bd6a550f5f047f1e
ljwoon1211 <ljwoon1211@gmail.com>
2025-02-12 14:51:42 +0900

    First commit

---
+++ src/Router.tsx
@@ -0,0 +1,57 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "./App";
+
+import { lazy,Suspense } from "react";
+// const ToDoList = lazy(() => import('./components/ToDoList'));
+
+// const Categories = lazy(() => import('./pages/Categories'));
+// const Coins = lazy(() => import('./pages/Coins'));
+// const Coin = lazy(() => import('./pages/Coin'));
+// const Chart = lazy(() => import('./pages/Chart'));
+// const Price = lazy(() => import('./pages/Price'));
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    // children: [
+    //   {
+    //     path: "",
+    //     element: (
+    //       <Suspense fallback={<div>Loading...</div>}>
+    //         {/* <ToDoList /> */}
+    //       </Suspense>
+    //     )
+    //   },
+    // ]
+  },
+    //   {
+    //     path: "btc/:coinId",
+    //     element: (
+    //       <Suspense fallback={<div>Loading...</div>}>
+    //         <Coin />
+    //       </Suspense>
+    //     ),
+    //     children: [
+    //       {
+    //         path: "chart",
+    //         element: (
+    //           <Suspense fallback={<div>Loading...</div>}>
+    //             <Chart />
+    //           </Suspense>
+    //         ),
+    //       },
+    //       {
+    //         path: "price",
+    //         element: (
+    //           <Suspense fallback={<div>Loading...</div>}>
+    //             <Price />
+    //           </Suspense>
+    //         ),
+    //       },
+    //     ],
+    //   },
+    ]
+);
+
+export default router;
