2ce6606adcae87148d3038eb73ab3336e7b8750f
ljwoon1211 <ljwoon1211@gmail.com>
2025-02-14 14:57:45 +0900

    Feat : Variants

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import React from 'react';
 import { useRecoilState } from 'recoil';
 import styled from 'styled-components';
@@ -10,20 +11,78 @@ const Wrapper = styled.div`
   justify-content: center;
   align-items: center;
 `
-const Box = styled.div`
+const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
+  /* background-color: white; */
+  background-color: rgba(255, 255,255,0.2);
   border-radius: 10px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  display: grid;
+  grid-template-columns: repeat(2,1fr);
+  border-radius: 40px;
 `;
 
+const Circle = styled(motion.div)`
+  width: 70px;
+  height: 70px;
+  background-color: white;
+  border-radius: 35px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  place-self: center;
+`;
+
+const myVars = {
+ start:{scale:0},
+ end: {
+  scale:1, rotateZ:360, transition:{
+    type:"spring",delay:0.5
+  }
+ }
+}
+
+const boxVariants = {
+  start: {
+    opacity: 0,
+    scale: 0.5,
+  },
+  end: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      duration: 0.5,
+      bounce: 0.5,
+      delayChildren: 0.5,
+      staggerChildren: 0.2,
+    },
+  },
+};
+const circleVariants = {
+  start: {
+    opacity: 0,
+    y: 10,
+  },
+  end: {
+    opacity: 1,
+    y: 0,
+  },
+};
 
 function App() {
  
   return (
     <Wrapper>
-      <Box />
+      {/* <Box transition={{duration:3}} animate={{borderRadius:"100px"}} /> */}
+      {/* 애니메이션에 객체를 만든 후 initial, animate로 객체의 정보를 가져올 수 있다. 
+        myVars 객체에 start, end를 만든 후 variants에 넣으면 사용가능
+      */}
+      <Box variants={boxVariants} initial="start" animate="end">
+        <Circle variants={circleVariants} initial="start" animate="end"/>
+        <Circle variants={circleVariants} initial="start" animate="end"/>
+        <Circle variants={circleVariants} initial="start" animate="end"/>
+        <Circle variants={circleVariants} initial="start" animate="end"/>
+      </Box>
     </Wrapper>
   )
 }
--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,5 @@
-import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
+import { createBrowserRouter } from 'react-router-dom';
 
 import { lazy,Suspense } from "react";
 import React from "react";
