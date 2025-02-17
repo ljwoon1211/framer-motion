a16039f324698d30d45f94c88d2af51e554ffa04
ljwoon1211 <ljwoon1211@gmail.com>
2025-02-17 21:12:07 +0900

    Feat : add Slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import { motion, useMotionValue, useScroll, useTransform } from 'framer-motion';
-import React, { useEffect, useRef } from 'react';
+import { AnimatePresence, motion, useMotionValue, useScroll, useTransform } from 'framer-motion';
+import React, { useEffect, useRef, useState } from 'react';
 import { useRecoilState } from 'recoil';
 import styled from 'styled-components';
 
 
 const Wrapper = styled(motion.div)`
-  height: 200vh;
+  height: 100vh;
   width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
   background: linear-gradient(135deg,rgb(2));
 `
 const Box = styled(motion.div)`
@@ -19,8 +20,13 @@ const Box = styled(motion.div)`
   background-color: rgba(255, 255,255,1);
   border-radius: 10px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-  display: grid;
-
+  /* display: grid; */
+  position: absolute;
+  top: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 28px;
 `;
 
 const Circle = styled(motion.div)`
@@ -65,64 +71,55 @@ const svg = {
   },
 };
 
+const box = {
+  entry:(isBack:boolean)=>({
+    x: isBack ? -500 : 500,
+    opacity: 0,
+    scale: 0,
+  }), 
+  center: {
+    x: 0,
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 1,
+    },
+  },
+  exit: (isBack:boolean)=>({ 
+    x: isBack? 500: -500, 
+    opacity: 0, 
+    scale: 0, 
+    transition: { duration: 1 } 
+  }),
+};
+
 function App() {
-  const biggerBoxRef = useRef<HTMLDivElement>(null)
-  const x = useMotionValue(0) 
-  const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
-  const gradient = useTransform(
-    x,
-    [-800, 800],
-    [
-      "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))",
-      "linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 178, 0))",
-    ]
-  );
-  const { scrollYProgress } = useScroll();
-  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
-  useEffect(() => {
-    function updateX() {
-      const xValue = x.get()
-      console.log("current X value :",xValue)
-    }
-    const unsubscribeX = x.on("change", updateX)
-    return () => {
-      unsubscribeX()
-    }
-  }, [x])
-  useEffect(() => {
-    function updateRotateZ() {
-      const rotateZValue = rotateZ.get()
-      console.log("current rotateZ value :",rotateZValue)
-    }
-    const unsubscribeX = x.on("change", updateRotateZ)
-    return () => {
-      unsubscribeX()
-    }
-  }, [rotateZ])
+  const [visible, setVisible] = useState(1);
+  const [isBack, setIsBack] = useState<boolean>(false);
+  const nextPlease = () =>{
+    setIsBack(false)
+    setVisible((prev)=>(prev === 10 ? 10: prev +1))
+  }
+  const prevPlease = () =>{
+    setIsBack(true) 
+    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+  }
   return (
-    <Wrapper style={{ background: gradient }} >
-      <Svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-        <motion.path
-          variants={svg}
-          initial="start"
-          animate="end"
-          transition={{
-            default: { duration: 5 },
-            fill: { duration: 1, delay: 3 },
-          }}
-          d="M224 373.1c-25.2-31.7-40.1-59.4-45-83.2-22.6-88 112.6-88 90.1 0-5.5 24.3-20.3 52-45 83.2zm138.2 73.2c-42.1 18.3-83.7-10.9-119.3-50.5 103.9-130.1 46.1-200-18.9-200-54.9 0-85.2 46.5-73.3 100.5 6.9 29.2 25.2 62.4 54.4 99.5-32.5 36.1-60.6 52.7-85.2 54.9-50 7.4-89.1-41.1-71.3-91.1 15.1-39.2 111.7-231.2 115.9-241.6 15.8-30.1 25.6-57.4 59.4-57.4 32.3 0 43.4 25.9 60.4 59.9 36 70.6 89.4 177.5 114.8 239.1 13.2 33.1-1.4 71.3-37 86.6zm47-136.1C280.3 35.9 273.1 32 224 32c-45.5 0-64.9 31.7-84.7 72.8C33.2 317.1 22.9 347.2 22 349.8-3.2 419.1 48.7 480 111.6 480c21.7 0 60.6-6.1 112.4-62.4 58.7 63.8 101.3 62.4 112.4 62.4 62.9 .1 114.9-60.9 89.6-130.2 0-3.9-16.8-38.9-16.8-39.6z">
-          </motion.path> 
-          </Svg>
-      {/* <Box transition={{duration:3}} animate={{borderRadius:"100px"}} /> */}
-      {/* 애니메이션에 객체를 만든 후 initial, animate로 객체의 정보를 가져올 수 있다. 
-        myVars 객체에 start, end를 만든 후 variants에 넣으면 사용가능
-      */}
-      <button onClick={()=>x.set(20)}>click me</button>
-      <BiggerBox ref={biggerBoxRef}>
-        <Box style={{x,rotateZ,scale}} drag="x"
-        dragSnapToOrigin>
-        </Box>
-      </BiggerBox>
+    <Wrapper >
+        <AnimatePresence>
+            <Box
+              custom={isBack}
+              variants={box}
+              initial="entry"
+              animate="center"
+              exit="exit"
+              key={visible}
+            >
+              {visible}
+            </Box>
+      </AnimatePresence>
+      <button onClick={nextPlease}>next</button>
+      <button onClick={prevPlease}>prev</button>
     </Wrapper>
   )
 }
