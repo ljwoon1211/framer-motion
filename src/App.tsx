9c8f1e68dc4c8d461aafdc1a55110ca6b93d134e
ljwoon1211 <ljwoon1211@gmail.com>
2025-02-17 21:18:14 +0900

    Feat : Add toggle

--- src/App.tsx
+++ src/App.tsx
@@ -30,10 +30,10 @@ const Box = styled(motion.div)`
 `;
 
 const Circle = styled(motion.div)`
-  width: 70px;
-  height: 70px;
-  background-color: white;
-  border-radius: 35px;
+  width: 100px;
+  height: 100px;
+  background-color: blue;
+  border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
   place-self: center;
 `;
@@ -93,34 +93,24 @@ const box = {
   }),
 };
 
+
+
 function App() {
-  const [visible, setVisible] = useState(1);
-  const [isBack, setIsBack] = useState<boolean>(false);
-  const nextPlease = () =>{
-    setIsBack(false)
-    setVisible((prev)=>(prev === 10 ? 10: prev +1))
-  }
-  const prevPlease = () =>{
-    setIsBack(true) 
-    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
-  }
+  const [clicked, setClicked] = useState(false);
+  const toggleClicked = () => setClicked((prev) => !prev);
   return (
-    <Wrapper >
-        <AnimatePresence>
-            <Box
-              custom={isBack}
-              variants={box}
-              initial="entry"
-              animate="center"
-              exit="exit"
-              key={visible}
-            >
-              {visible}
-            </Box>
-      </AnimatePresence>
-      <button onClick={nextPlease}>next</button>
-      <button onClick={prevPlease}>prev</button>
-    </Wrapper>
+    <Wrapper onClick={toggleClicked}>
+    <Box>
+      {!clicked ? (
+        <Circle layoutId="circle" style={{ borderRadius: 50 }} />
+      ) : null}
+    </Box>
+    <Box>
+      {clicked ? (
+        <Circle layoutId="circle" style={{ borderRadius: 0, scale: 2 }} />
+      ) : null}
+    </Box>
+  </Wrapper>
   )
 }
 
